846b16b9caba52c892b32fc462b6cbb8e9bcd539
Cristian Monforte <cristian.monforte@gmail.com>
2015-02-13 22:28:14 +0100

    tratado error maxlikes

--- src/main/java/com/getinch/retrogram/model/LikeResponse.java
+++ src/main/java/com/getinch/retrogram/model/LikeResponse.java
@@ -8,9 +8,17 @@ public class LikeResponse {
         return meta.code.equals(200);
     }
 
+    public boolean isMaxReached(){return meta.code.equals(429);}
+
+    public String getErrorType(){ return meta.error_type;}
+
+    public String getErrorMessage(){ return meta.error_message;}
+
     private static class Meta {
 
         private Integer code;
+        private String error_type;
+        private String error_message;
 
     }
 
